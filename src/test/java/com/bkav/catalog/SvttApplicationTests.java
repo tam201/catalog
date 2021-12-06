f741a5ed2c9f7f120201b16079e3b4666a8e8cb6
baonq <baonq@bkav.com>
2021-12-06 15:38:53 +0700

    Initial Commit

---
+++ src/test/java/com/bkav/catalog/SvttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bkav.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
